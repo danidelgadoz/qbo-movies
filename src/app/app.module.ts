87bee078a78b78ae580cd0bd74a0f2e4e819f3e3
Dandel <dedd1993@gmail.com>
2021-02-19 21:35:18 -0500

    implementacion de core module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AppInterceptor } from './app.interceptor';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { LoginComponent } from './login/login.component';
@@ -22,8 +19,6 @@ import { SharedModule } from './shared/shared.module';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule,
-    HttpClientModule,
     SharedModule,
     MovieModule,
     RouterModule.forRoot([
@@ -33,9 +28,6 @@ import { SharedModule } from './shared/shared.module';
       { path: '', redirectTo: 'movies', pathMatch: 'full' },
       { path: '**', redirectTo: 'not-found' },
     ])
-  ],
-  providers: [
-    { provide: HTTP_INTERCEPTORS, useClass: AppInterceptor, multi: true },
   ],
   bootstrap: [AppComponent]
 })
