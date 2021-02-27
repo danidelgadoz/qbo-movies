8de489dac09147ef9a4a087c5a15fd800b47d5c3
Dandel <dedd1993@gmail.com>
2021-02-26 21:00:11 -0500

    agregar guard para validar la sesion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { WelcomeComponent } from './welcome/welcome.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { AuthGuard } from './auth.guard';
 
 const appRoutes: Routes = [
   { path: 'login', component: LoginComponent },
@@ -13,6 +14,7 @@ const appRoutes: Routes = [
   {
     path: 'movies',
     loadChildren: () => import('./movie/movie.module').then(m => m.MovieModule),
+    canActivate: [AuthGuard]
   },
   { path: 'not-found', component: NotFoundComponent },
   { path: '', redirectTo: 'login', pathMatch: 'full' },
@@ -22,6 +24,7 @@ const appRoutes: Routes = [
 @NgModule({
   imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule],
+  providers: [AuthGuard]
 })
 export class AppRoutingModule {
   static components = [
--- src/app/core/auth.service.ts
+++ src/app/core/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -14,6 +15,16 @@ export class AuthService {
       username: username,
       password: password,
     };
-    return this.http.post('http://localhost:3000/oauth/token', body);
+    return this.http.post('http://localhost:3000/oauth/token', body)
+      .pipe(
+        tap((response) => {
+          const responseInStringFormat = JSON.stringify(response);
+          localStorage.setItem('session', responseInStringFormat);
+        })
+      );
+  }
+
+  isLogged(): boolean {
+    return localStorage.getItem('session') ? true : false;
   }
 }
