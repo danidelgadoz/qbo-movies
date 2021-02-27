17618eb39740db2bf5bee215dcc4457cf02ec144
Dandel <dedd1993@gmail.com>
2021-02-26 20:02:39 -0500

    refactor encapsulando los modulos de angular material

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,41 +3,18 @@ import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { FlexLayoutModule } from '@angular/flex-layout';
-import {MatCardModule} from '@angular/material/card';
-import {MatButtonModule} from '@angular/material/button';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MatNativeDateModule} from '@angular/material/core';
-import {MatSelectModule} from '@angular/material/select';
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
-
-const MATERIAL_MODULES = [
-  MatCardModule,
-  MatButtonModule,
-  MatToolbarModule,
-  MatIconModule,
-  MatFormFieldModule,
-  MatInputModule,
-  MatDatepickerModule,
-  MatNativeDateModule,
-  MatSelectModule,
-  MatProgressSpinnerModule
-];
-
+import { MaterialModule } from './material.module';
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
   ],
   exports: [
     FormsModule,
     ReactiveFormsModule,
     FlexLayoutModule,
-    ...MATERIAL_MODULES
+    MaterialModule
   ]
 })
 export class SharedModule { }
